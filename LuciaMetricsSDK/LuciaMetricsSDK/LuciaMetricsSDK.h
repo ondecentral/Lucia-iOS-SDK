37eec49b08e163228d2bbf78ddf77666ea9ebb23
Stefan Progovac <sprogovac@gmail.com>
2025-09-04 13:20:05 -0400

    NEW SDK Template

---
+++ LuciaMetricsSDK/LuciaMetricsSDK/LuciaMetricsSDK.h
@@ -0,0 +1,18 @@
+//
+//  LuciaMetricsSDK.h
+//  LuciaMetricsSDK
+//
+//  Created by Stefan Progovac on 9/4/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LuciaMetricsSDK.
+FOUNDATION_EXPORT double LuciaMetricsSDKVersionNumber;
+
+//! Project version string for LuciaMetricsSDK.
+FOUNDATION_EXPORT const unsigned char LuciaMetricsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LuciaMetricsSDK/PublicHeader.h>
+
+
---
+++ LuciaMetricsSDK/LuciaMetricsSDKTests/LuciaMetricsSDKTests.swift
@@ -0,0 +1,17 @@
+//
+//  LuciaMetricsSDKTests.swift
+//  LuciaMetricsSDKTests
+//
+//  Created by Stefan Progovac on 9/4/25.
+//
+
+import Testing
+@testable import LuciaMetricsSDK
+
+struct LuciaMetricsSDKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
